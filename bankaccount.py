ed73e40f2b4291fe54ef830222c77a388f34489e
Harshavardhan Aravala <harsha@Harshavardhans-MacBook-Pro.local>
2026-01-21 16:56:57 +0530

    Added functionality for multiple accounts

--- bankaccount.py
+++ bankaccount.py
@@ -18,28 +18,42 @@ class BankAccount:
     def get_balance(self):
         print(self.balance) 
 
-
+accounts = {}
 
         
 while True:
     choice = int(input("Enter 1 for a new account, 2 for depositing, 3 for withdrawing, 4 for getting balance, 5 to quit menu"))
     if choice == 1:
         name = input("Please enter account name:")
-        name_acc = BankAccount(name, 0)
+        i_amount = int(input("Enter starting balance:"))
+        name_acc = BankAccount(name,i_amount)
+        accounts[name] = name_acc
         continue
     
     elif choice == 2:
-        damount = int(input("How much would you like to deposit?"))
-        name_acc.deposit(damount)
+        index = input("Whose account do you want to deposit in?(Give account name) ")
+        if index in accounts:
+            damount = int(input("How much would you like to deposit?"))
+            accounts[index].deposit(damount)
+        else:
+            print("Enter a valid account name.")
         continue
     
     elif choice ==3:
         wamount = int(input("How much would you like to withdraw?"))
-        name_acc.withdraw(wamount)
+        index = input("Whose account do you want to withdraw from?(Give account name) ")
+        if index in accounts:
+            accounts[index].withdraw(wamount)
+        else:
+            print("Enter a valid account name.")
         continue
     
     elif choice == 4:
-        name_acc.get_balance()
+        index = input("Which account balance would you like to see? ")
+        if index in accounts:
+            accounts[index].get_balance()
+        else:
+            print("Enter a valid account name.")
         continue
     
     elif choice == 5:
