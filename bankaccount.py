be6351fd53ad5435e3b84f8edae22c453249a803
Harshavardhan Aravala <harsha@Harshavardhans-MacBook-Pro.local>
2026-02-23 18:42:37 +0530

    Connected to database fully

--- bankaccount.py
+++ bankaccount.py
@@ -1,6 +1,6 @@
 import datetime
 import mysql.connector 
-from config import DB_CONFIG
+from realconfig import DB_CONFIG
 now = datetime.datetime.now()
 
 def setup_database():
@@ -45,8 +45,6 @@ def create_account(name, initial_balance):
     mydb.close()
 
     return new_id
-account_id = create_account("Harsha",1000)
-print(account_id)
 
                     
 def get_account_balance(account_id):
@@ -63,37 +61,61 @@ def get_account_balance(account_id):
     else:
         return None
     
+def update_account_balance(account_id,new_balance):
+     
+     mydb = mysql.connector.connect(**DB_CONFIG)
+     mycursor = mydb.cursor()
+     sql = "UPDATE accounts SET balance = %s WHERE id = %s"
+     value = (new_balance,account_id)
+     mycursor.execute(sql,value)
+     mydb.commit()
+     mycursor.close()
+     mydb.close()
+
+
+def account_exists(account_id):
+
+    mydb = mysql.connector.connect(**DB_CONFIG)
+    mycursor = mydb.cursor()
+    sql = "SELECT COUNT(*) FROM accounts WHERE id = %s"
+    value = (account_id,)
+    mycursor.execute(sql,value)
+    result =mycursor.fetchone()
+    count = result[0]
+    mycursor.close()
+    mydb.close()
+    if count == 1:
+        return True
+    else:
+        return False
     
+def log_transaction(account_id,trans_type,amount,description):
+
+    mydb = mysql.connector.connect(**DB_CONFIG)
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO transactions(account_id,type,amount,description) VALUES(%s,%s,%s,%s)"
+    params = (account_id,trans_type,amount,description)
+    mycursor.execute(sql,params) 
+    mydb.commit()
+    mycursor.close()
+    mydb.close()
+
+def get_account_statement(account_id):
+    mydb = mysql.connector.connect(**DB_CONFIG)
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM transactions WHERE account_id = %s ORDER BY timestamp DESC"
+    value = (account_id,)
+    mycursor.execute(sql,value)
+    result = mycursor.fetchall()
+    mycursor.close()
+    mydb.close()
+    return result
 
 
 
 
 
 
-
-class BankAccount:
-    
-    def __init__(self,name,balance):
-        self.name = name
-        self.balance = balance
-        self.transactions = []
-    
-    def deposit(self,amount):
-        if amount >=0:
-            self.balance += amount
-        else:
-            print("Please enter a valid number")
-
-    def withdraw(self,amount):
-        if self.balance>0 and self.balance >=amount:
-            self.balance-=amount
-        else:
-            print("The requested funds are not available.")
-    
-    def get_balance(self):
-        print(self.balance) 
-
-accounts = {}
 
 print("Main Menu")
 print("Press the numbers for the following actions")
@@ -109,45 +131,50 @@ while True:
     if choice == 1:
         name = input("Please enter account name:")
         i_amount = int(input("Enter starting balance:"))
-        name_acc = BankAccount(name,i_amount)
-        accounts[name] = name_acc
+        new_id = create_account(name,i_amount)
+        print(f"Your account has been sucessfully created. Your account ID is {new_id}.")
         continue
     
     elif choice == 2:
-        index = input("Whose account do you want to deposit in?(Give account name) ")
-        if index in accounts:
+        index = input("Whose account do you want to deposit in?(Give account ID) ")
+        if account_exists(index):
             damount = int(input("How much would you like to deposit?"))
-            accounts[index].deposit(damount)
-            accounts[index].transactions.append("You have succesfully deposited {} into {}'s account on {}".format(damount,index,now))
-            print(f"You have succesfully deposited {damount} into {index}'s account.")
+            if damount > 0:
+                new_amount = get_account_balance(index) + damount
+                update_account_balance(index,new_amount)
+                log_transaction(index,"DEPOSIT",damount,f"Deposited ₹{damount}. ")
+                print(f"You have succesfully deposited {damount} into {index}'s account.")
         else:
             print("Enter a valid account name.")
         continue
     
     elif choice ==3:
         wamount = int(input("How much would you like to withdraw?"))
-        index = input("Whose account do you want to withdraw from?(Give account name) ")
-        if index in accounts:
-            accounts[index].withdraw(wamount)
-            accounts[index].transactions.append("You have successfully withdrawed {} from {}'s account on {}".format(wamount,index,now))
-            print(f"You have successfully withdrawed {wamount} from {index}'s account.")
+        index = input("Whose account do you want to withdraw from?(Give account ID) ")
+        if account_exists(index):
+            if wamount <= get_account_balance(index):
+                newamount = get_account_balance(index) - wamount
+                update_account_balance(index,newamount)
+                log_transaction(index,"WITHDRAW",wamount,f"Withdrawed ₹{wamount}.")
+                print(f"You have successfully withdrawed {wamount} from {index}'s account.")
         else:
             print("Enter a valid account name.")
         continue
     
     elif choice == 4:
         index = input("Which account balance would you like to see? ")
-        if index in accounts:
-            accounts[index].get_balance()
+        if account_exists(index):
+            get_account_balance(index)
         else:
             print("Enter a valid account name.")
         continue
     
     elif choice == 5:
         index = input("Which account statement would you like to see? ")
-        if index in accounts:
-            for transaction in accounts[index].transactions:
-                print (transaction)
+        if account_exists(index):
+            transactions = get_account_statement(index)
+            for transaction in transactions:
+                print(f"{transaction[5]};Type:{transaction[2]},Timestamp:{transaction[4]}")
         else:
             print("Enter a valid account name.")
         continue
